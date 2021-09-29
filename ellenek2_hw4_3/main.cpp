dcc79e420ebd41d3ec9b146c4a6d1d193e5080c5
ellenkirsh <ellenek2@illinois.edu>
2021-09-29 18:14:37 -0500

    Update main.cpp

--- ellenek2_hw4_3/main.cpp
+++ ellenek2_hw4_3/main.cpp
@@ -6,9 +6,37 @@
 //
 
 #include <iostream>
+#include<random>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int lottery[5], user[5];
+    int sum=0;
+    for(int i=0;i<5; i++){
+        lottery[i] = rand()% 10 + 1;
+    }
+    cout << "Enter your 5 lottery numbers: \n";
+    for(int i=0; i<5; i++){
+        cin >> user[i];
+        cout << "\n";
+    }
+    cout << "The winning numbers are: ";
+    for(int i=0; i<5; i++){
+        cout << lottery[i] << " ";
+    }
+    cout << "\n";
+    for(int i=0; i<5; i++){
+        if((lottery[i]==user[i]) || (lottery[i] == user[i]+1) ||(lottery[i]==user[i]-1)){
+            int x = 1;
+            sum = sum + x;
+        }else{
+            int x = 0;
+        }
+    }
+    if(sum == 5){
+        cout << "YOU ARE A BIG PRIZE WINNER!\n";
+    }else{
+        cout << "You guessed "<< sum << " number(s) within 1 of the winning numbers\n";
+    }
     return 0;
 }
